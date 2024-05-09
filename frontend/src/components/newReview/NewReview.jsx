ed40a86bde311b335b80ab8fedaaa5b7ed8397a3
Gustavo Rivera <griverab@unal.edu.co>
2024-05-08 23:18:06 -0500

    completacion de flujo (registro)  reseñas laborales

--- frontend/src/components/newReview/NewReview.jsx
+++ frontend/src/components/newReview/NewReview.jsx
@@ -1,6 +1,6 @@
 import "./newReview.scss";
 import { AuthContext } from "../../context/authContext";
-import { useContext, useState } from "react";
+import { useRef, useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -13,9 +13,12 @@ const NewReview = () => {
     setBandera(!bandera);
   };
 
+  //mensajes de error formato reseñas de producto
   const [err, setErr] = useState(null);
+  //mensajes de error formato reseñas laborales
+  const [err2, setErr2] = useState(null);
 
-  //variable de captura de datos
+  //variable de captura de datos formato reseña de producto
   const [inputs, setInputs] = useState({
     company_name: "",
     product_name: "",
@@ -25,19 +28,37 @@ const NewReview = () => {
     service_score: "",
     idUser: currentUser.idUser,
   });
+  //variable de captura de datos formato reseña laboral
+  const [inputsJR, setInputsJR] = useState({
+    company_name: "",
+    occupation_name: "",
+    review_j: "",
+    work_environment: "",
+    growth_opp: "",
+    salary: "",
+    idUser: currentUser.idUser,
+  });
+
   //ruta base del servidor backend
   const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_SERVER_URL,
   });
 
-  const handleChange = (e) => {
+  //actualizar el valor de los inputs cuando se presenten cambios (formato reseña de producto)
+
+  const handleChangeCustomerReview = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  //actualizar el valor de los inputs cuando se presenten cambios (formato reseña laboral)
+  const handleChangeJobReview = (e) => {
+    setInputsJR((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const navigate = useNavigate();
 
   //funcion para enviar la peticion al servidor backend
-  const handleClick = async (e) => {
+  const handleClickCustomerReview = async (e) => {
     e.preventDefault();
     //validar existencia del obj currentUser
     if (currentUser === null) {
@@ -89,6 +110,58 @@ const NewReview = () => {
     }
   };
 
+  //referencia al formulario de reseña laboral
+  const refFormJobReview = useRef(null);
+
+  //funcion para enviar la peticion al servidor backend
+  const handleClickJobReview = async (e) => {
+    e.preventDefault();
+    //validar existencia del obj currentUser
+    if (currentUser === null) {
+      if (window.confirm("Debes iniciar sesión para publicar una reseña")) {
+        navigate("/login");
+      }
+    } else {
+      console.log(inputsJR);
+      console.log(currentUser);
+      //console.log(form);
+      if (!refFormJobReview.current.checkValidity()) {
+        setErr2("Por favor, rellena todos los campos correctamente");
+        return;
+      }
+      if (
+        inputsJR.company_name.trim() === "" ||
+        inputsJR.occupation_name.trim() === "" ||
+        inputsJR.review_j.trim() === ""
+      ) {
+        setErr2("No dejes campos vacios!");
+        return;
+      }
+      try {
+        //confirm devuelve true si el usuario da click en ok
+        if (window.confirm("aceptar para crear reseña")) {
+          //se realiza la peticion a la bd
+          await axiosInstance.post("/server/postsj/add-job-review", inputsJR);
+          //si el registro es exitoso se notifica que la reseña esta en validacion
+          alert("La reseña ha sido registrada para validacion");
+          //reset inputs
+          setInputsJR({
+            company_name: "",
+            occupation_name: "",
+            review_j: "",
+            work_environment: "",
+            growth_opp: "",
+            salary: "",
+          });
+          setErr2(null);
+        }
+      } catch (err2) {
+        console.log(err2);
+        //setErr(err.response.data);
+      }
+    }
+  };
+
   return (
     <div className="newReview">
       <div className="container">
@@ -117,7 +190,7 @@ const NewReview = () => {
                       className="input_name"
                       type="text"
                       name="company_name"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       required={true}
                       pattern=".{1,40}"
                       value={inputs.company_name}
@@ -130,7 +203,7 @@ const NewReview = () => {
                       className="input_name"
                       type="text"
                       name="product_name"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       required={true}
                       pattern=".{1,40}"
                       value={inputs.product_name}
@@ -142,7 +215,7 @@ const NewReview = () => {
                     <textarea
                       className="input_name"
                       name="review_c"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       required={true}
                       pattern=".{1,200}"
                       value={inputs.review_c}
@@ -162,7 +235,7 @@ const NewReview = () => {
                       className="input_value"
                       name="quality_score"
                       type="number"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       value={inputs.quality_score}
                       required={true}
                       min="1"
@@ -176,7 +249,7 @@ const NewReview = () => {
                       className="input_value"
                       name="price_score"
                       type="number"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       value={inputs.price_score}
                       required={true}
                       min="1"
@@ -190,7 +263,7 @@ const NewReview = () => {
                       className="input_value"
                       name="service_score"
                       type="number"
-                      onChange={handleChange}
+                      onChange={handleChangeCustomerReview}
                       value={inputs.service_score}
                       required={true}
                       min="1"
@@ -201,7 +274,7 @@ const NewReview = () => {
                 </div>
                 <div className="bottom">
                   <span className="err">{err}</span>
-                  <button onClick={handleClick}>Publicar</button>
+                  <button onClick={handleClickCustomerReview}>Publicar</button>
                 </div>
               </form>
             </div>
@@ -210,75 +283,103 @@ const NewReview = () => {
           <>
             <hr />
             <div className="job-review">
-              <div className="top">
-                <div className="input">
-                  <span className="input_name">Nombre empresa:</span>
-                  <input
-                    className="input_name"
-                    type="text"
-                    name="company_name"
-                  />
-                  <span className="warning">(40 caracteres)</span>
-                </div>
-                <div className="input">
-                  <span className="input_name">Cargo:</span>
-                  <input
-                    className="input_name"
-                    type="text"
-                    name="ocupation_name"
-                  />
-                  <span className="warning">(40 caracteres)</span>
-                </div>
-                <div className="input">
-                  <span className="input_name">Descripcion:</span>
-                  <textarea
-                    className="input_name"
-                    name="comments"
-                    rows="5"
-                    cols="60"
-                  ></textarea>
-                  <span className="warning">(200 caracteres)</span>
-                </div>
-                <hr />
-                <div className="calificacion">
-                  <span>(1 menor calificacion - 5 mayor calificacion )</span>
-                </div>
+              <form ref={refFormJobReview}>
+                <div className="top">
+                  <div className="input">
+                    <span className="input_name">Nombre empresa:</span>
+                    <input
+                      className="input_name"
+                      type="text"
+                      name="company_name"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.company_name}
+                      required={true}
+                      pattern=".{1,40}"
+                    />
+                    <span className="warning">(40 caracteres)</span>
+                  </div>
+                  <div className="input">
+                    <span className="input_name">Cargo:</span>
+                    <input
+                      className="input_name"
+                      type="text"
+                      name="occupation_name"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.occupation_name}
+                      required={true}
+                      pattern=".{1,200}"
+                    />
+                    <span className="warning">(200 caracteres)</span>
+                  </div>
+                  <div className="input">
+                    <span className="input_name">Descripcion:</span>
+                    <textarea
+                      className="input_name"
+                      name="review_j"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.review_j}
+                      required={true}
+                      pattern=".{1,200}"
+                      rows="5"
+                      cols="60"
+                    ></textarea>
+                    <span className="warning">(200 caracteres)</span>
+                  </div>
+                  <hr />
+                  <div className="calificacion">
+                    <span>(1 menor calificacion - 5 mayor calificacion )</span>
+                  </div>
 
-                <div className="input">
-                  <span className="input_name">Ambiente laboral</span>
-                  <input
-                    className="input_value"
-                    type="number"
-                    min="1"
-                    max="5"
-                  />
-                  <span className="warning"></span>
+                  <div className="input">
+                    <span className="input_name">Ambiente laboral</span>
+                    <input
+                      className="input_value"
+                      type="number"
+                      name="work_environment"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.work_environment}
+                      required={true}
+                      min="1"
+                      max="5"
+                    />
+                    <span className="warning"></span>
+                  </div>
+                  <div className="input">
+                    <span className="input_name">
+                      Oportunidades de crecimiento
+                    </span>
+                    <input
+                      className="input_value"
+                      type="number"
+                      name="growth_opp"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.growth_opp}
+                      required={true}
+                      min="1"
+                      max="5"
+                    />
+                    <span className="warning"></span>
+                  </div>
+                  <div className="input">
+                    <span className="input_name">Salario</span>
+                    <input
+                      className="input_value"
+                      type="number"
+                      name="salary"
+                      onChange={handleChangeJobReview}
+                      value={inputsJR.salary}
+                      required={true}
+                      min="1"
+                      max="5"
+                    />
+                    <span className="warning"></span>
+                  </div>
                 </div>
-                <div className="input">
-                  <span className="input_name">
-                    Oportunidades de crecimiento
-                  </span>
-                  <input
-                    className="input_value"
-                    type="number"
-                    min="1"
-                    max="5"
-                  />
-                  <span className="warning"></span>
-                </div>
-                <div className="input">
-                  <span className="input_name">Salario</span>
-                  <input
-                    className="input_value"
-                    type="number"
-                    min="1"
-                    max="5"
-                  />
-                  <span className="warning"></span>
-                </div>
-              </div>
+              </form>
               <div className="bottom">
-                <button>Publicar</button>
+                <span className="err">{err2}</span>
+
+                <button onClick={handleClickJobReview}>Publicar</button>
               </div>
             </div>
           </>
--- server/controllers/postj.js
+++ server/controllers/postj.js
@@ -27,4 +27,35 @@ export const getUserReviews = (req, res) => {
   //traer las reseñas laborales asociadas a un usuario especifico
 };
 
+
+export const addPost = (req, res) => {
+
+  //establecer fecha de creacion yy/mm/dd hh:mm:ss
+  const d = Date.now();
+  const date = new Date(d); 
+  const created_at = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()
+  +" " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+
+  const q =
+    "INSERT INTO Job_review (`Created_At`, `Company_Name`, `Occupation` , `Review_J`, `Work_Env_Score`, `Growth_Opp_Score`,`Salary_Score`, `Available`, `User_idUser`, `Company_idCompany`) VALUES (?)";
+  const values = [
+    created_at,
+    req.body.company_name,
+    req.body.occupation_name,
+    req.body.review_j,
+    req.body.work_environment,
+    req.body.growth_opp,
+    req.body.salary,
+    0,
+    req.body.idUser,
+    1,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("La reseña ha sido agregada.");
+  });
+};
+
   
