f9d1acf733de661bb1d1d15d90f72d3963d60643
Gustavo Rivera <griverab@unal.edu.co>
2024-05-18 22:20:27 -0500

    variable de entorno DOMINIO con nombre de dominio

--- server/config.js
+++ server/config.js
@@ -7,6 +7,7 @@ export const PORT = process.env.PORT || 8800;
 export const BACKEND_URL = process.env.BACKEND_URL;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const DOMINIO = process.env.DOMINIO;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
