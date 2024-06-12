43dc73d11fd4b7ca7d28981a3edabb0eb6d439a6
Javier Santiago Vargas Parra <92332997+JavierVargas0112@users.noreply.github.com>
2024-06-12 12:56:33 -0500

    Likes Back

--- server/controllers/like.js
+++ server/controllers/like.js
@@ -15,6 +15,7 @@ export function addCostumerLike(req, res) {
     db.query("SELECT * FROM Likes WHERE userId = ? AND costumerId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
         if(data.length == 0) {
             db.query("DELETE FROM Dislikes WHERE userId = ? AND costumerId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+                db.query("UPDATE Costumer_review set Dislikes = Dislikes - 1 WHERE idCostumer_review = ?", [req.body.costumerId]);
                 if(err) return res.status(404).send(err)
             });}
         else return res.status(404).send("Ya existe un like para este usuario y costumer")
@@ -22,7 +23,13 @@ export function addCostumerLike(req, res) {
 
     const q = "INSERT INTO Likes (userId, reviewType ,costumerId ) VALUES (?, 'Costumer', ? )"  
             db.query(q, [req.body.userId, req.body.costumerId], (err, data) => {
-                if(data) return res.status(200).send("Like agregado")
+                if(data) {
+
+                    db.query("UPDATE Costumer_review set Likes = Likes + 1 WHERE idCostumer_review = ?", [req.body.costumerId], (err, data) => {
+                        return res.status(200).send("Like agregado")
+                    })
+                    
+                }
                 else return res.status(404).send(err)
             }); 
 }
@@ -30,28 +37,38 @@ export function addCostumerLike(req, res) {
 
 export function addJobLike(req, res) {
 
-    db.query("SELECT * FROM Likes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+    db.query("SELECT * FROM Dislikes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.jobId], (err, data) => {
         if(data.length == 0) {
-            db.query("DELETE FROM Dislikes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+            db.query("DELETE FROM Dislikes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.jobId], (err, data) => {
+                db.query("UPDATE Job_review set Dislikes = Dislikes - 1 WHERE idJob_review = ?", [req.body.jobId]);
                 if(err) return res.status(404).send(err)
             });}
         else return res.status(404).send("Ya existe un like para este usuario y costumer")
     })
 
     const q = "INSERT INTO Likes (userId, reviewType , jobId ) VALUES (?, 'Job', ? )"  
-            db.query(q, [req.body.userId, req.body.costumerId], (err, data) => {
-                if(data) return res.status(200).send("Like agregado")
+            db.query(q, [req.body.userId, req.body.jobId], (err, data) => {
+                if(data) {
+
+                    db.query("UPDATE Job_review set Likes = Likes + 1 WHERE idJob_review = ?", [req.body.jobId], (err, data) => {
+                        return res.status(200).send("Like agregado")
+                    })
+                    
+                }
                 else return res.status(404).send(err)
             });
 }
+
+    
 
 
 
 export function addCostumerDislike(req, res) {
 
-    db.query("SELECT * FROM Dislikes WHERE userId = ? AND costumerId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+    db.query("SELECT * FROM Likes WHERE userId = ? AND costumerId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
         if(data.length == 0) {
             db.query("DELETE FROM Likes WHERE userId = ? AND costumerId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+                db.query("UPDATE Costumer_review set Likes = Likes - 1 WHERE idCostumer_review = ?", [req.body.costumerId]);
                 if(err) return res.status(404).send(err)
             });}
         else return res.status(404).send("Ya existe un dislike para este usuario y costumer")
@@ -59,9 +76,13 @@ export function addCostumerDislike(req, res) {
 
     const q = "INSERT INTO Dislikes (userId, reviewType ,costumerId ) VALUES (?, 'Costumer', ? )"  
             db.query(q, [req.body.userId, req.body.costumerId], (err, data) => {
-                if(data) return res.status(200).send("Dislike agregado")
+                if(data) {
+                    db.query("UPDATE Costumer_review set Dislikes = Dislikes + 1 WHERE idCostumer_review = ?", [req.body.costumerId]);
+                    return res.status(200).send("Dislike agregado")
+                }
                 else return res.status(404).send(err)
             }); 
+
 }
 
 
@@ -69,7 +90,8 @@ export function addJobDislike(req, res) {
 
     db.query("SELECT * FROM Dislikes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
         if(data.length == 0) {
-            db.query("DELETE FROM Likes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+            db.query("DELETE FROM Dislkes WHERE userId = ? AND jobId = ?", [req.body.userId, req.body.costumerId], (err, data) => {
+                db.query("UPDATE Job_review set Likes = Likes - 1 WHERE idJob_review = ?", [req.body.costumerId]);
                 if(err) return res.status(404).send(err)
             });}
         else return res.status(404).send("Ya existe un Dislike para este usuario y costumer")
@@ -77,7 +99,12 @@ export function addJobDislike(req, res) {
 
     const q = "INSERT INTO Dislikes (userId, reviewType , jobId ) VALUES (?, 'Job', ? )"  
             db.query(q, [req.body.userId, req.body.costumerId], (err, data) => {
-                if(data) return res.status(200).send("Dislike agregado")
+
+
+                if(data) {
+                    db.query("UPDATE Job_review set Dislikes = Dislikes + 1 WHERE idJob_review = ?", [req.body.jobId]);
+                    return res.status(200).send("Dislike agregado")
+                }
                 else return res.status(404).send(err)
             });
 }
