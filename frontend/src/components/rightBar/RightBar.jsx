3b74266caa40f857bbaf45eaeba21df43d7c16f8
Gustavo Rivera <griverab@unal.edu.co>
2024-08-25 22:06:58 -0500

    edicion pag_ppal, sobre_nosotros

--- frontend/src/components/rightBar/RightBar.jsx
+++ frontend/src/components/rightBar/RightBar.jsx
@@ -12,52 +12,58 @@ function RightBar() {
     <div className="rightBar">
       <div className="container">
         {/**validar si el usuario tiene sesion activa */}
-        {
-          currentUser ? (
-            <>
-              <div className="item">
-                <div className="header">
-                  <AccessTimeRoundedIcon />
-                  <span><a href="https://manualtecnicocompanyrank.blogspot.com/" target="_blank">Manual Tecnico</a></span>
-                  <span><a href="https://drive.google.com/file/d/1whnHhO5hDOvw34wpN0WTC5vS9owQJGPC/view?usp=drive_link" target="_blank">Manual de Ususario</a></span>
-                </div>
-                <ul>
-                
-                  {/**
+        {currentUser ? (
+          <>
+            <div className="item">
+              <div className="header">
+                <a
+                  href="https://manualtecnicocompanyrank.blogspot.com/"
+                  target="_blank"
+                >
+                  Manual Tecnico
+                </a>
+                <a
+                  href="https://drive.google.com/file/d/1whnHhO5hDOvw34wpN0WTC5vS9owQJGPC/view?usp=drive_link"
+                  target="_blank"
+                >
+                  Manual de Ususario
+                </a>
+              </div>
+              <ul>
+                {/**
                  * implementar esto con una consulta a la bd usando
                  * currentUser
                  * {props.searches && 
                 props.searches.map((search) => (
                   <li>{search}</li>
                 ))} */}
-                </ul>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="item">
+              <div className="header">
+                <LoginRoundedIcon />
+                <span>Registrate para ver más</span>
               </div>
-            </>
-          ) : (
-            <>
-              <div className="item">
-                <div className="header">
-                  <LoginRoundedIcon />
-                  <span>Registrate para ver más</span>
-                </div>
-                <p>
-                  Consulta información de diversas empresas colombianas y sus
-                  principales valoraciones, ¡ no esperes más y registrate !
-                </p>
+              <p>
+                Consulta información de diversas empresas colombianas y sus
+                principales valoraciones, ¡ no esperes más y registrate !
+              </p>
+            </div>
+            <div className="item">
+              <div className="header">
+                <LoyaltyRoundedIcon />
+                <span>Suscripción</span>
               </div>
-              <div className="item">
-                <div className="header">
-                  <LoyaltyRoundedIcon />
-                  <span>Suscripción</span>
-                </div>
-                <p>
-                  Suscríbete a nuestra plataforma y obtén todos los beneficios
-                  de ser miembro.
-                </p>
-              </div>
-            </>
-          )
-        }
+              <p>
+                Suscríbete a nuestra plataforma y obtén todos los beneficios de
+                ser miembro.
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
